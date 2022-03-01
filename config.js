84816cf969d4b07a39d1cd005b896a886225a45c
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-01 12:54:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -35,8 +35,8 @@ function pickRandom(list) {
 
 global.DeveloperMode = 'false' //Buat Mode True False Moderator
 global.linkGC = ['https://chat.whatsapp.com/LcgnBtXnHbeJx6VOgQyoxw'] // ganti jadi group lu
-global.owner = ['6281215320837', '62831433937633', '6283143393763', '6288217277973', '62831433937630', '6285704954639', '62895336282144', '6285712420674'] // Put your number here
-global.mods = ['6285231651905'] // Want some help?
+global.owner = ['6282361160044', '6285212023766', '6282361160044', '6285212023766', '6282361160044', '6285212023766', '6282361160044', '6285212023766'] // Put your number here
+global.mods = ['6282361160044'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -73,12 +73,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '🌙 Loli_Botz\nwa.me/6281215320837\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @anandaganz7_\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
-global.author = '۰ ⸼'
+global.packname = 'ShiroBotz.'
+global.author = 'hehe'
 
 // silakan di ganti klo mau
-global.botwm = `🎮 Loli_Botz By Ananda_ツ`//UBAH JADI NAMAMU
-global.wm = `꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Lord_Ananda_ツ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷`
+global.botwm = `hyzer.`//UBAH JADI NAMAMU
+global.wm = `hyzer.`
 global.botdate = `❏ ${wktu}\n❏ ${week} ${date}`
 global.imagebot = `https://telegra.ph/file/793b6accf3758f092742a.jpg`
 global.logo = fs.readFileSync('./src/logo3.jpg')
